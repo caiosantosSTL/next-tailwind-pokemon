db2ae272c75ea7f0bb2a683ac7ca01f74bff00b1
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-15 15:04:03 -0600

    footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(params) {
         <>
 
             <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline" target="_blank">Flowbite™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline" target="_blank" rel="noopener noreferrer">Flowbite™</a>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 sm:mt-0">
                     <li>
